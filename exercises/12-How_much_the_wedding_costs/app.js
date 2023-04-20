e967c01359a62da481047e17f8951681af22d0ee
Aleix Aguilar <nitjac210@gmail.com>
2023-04-20 19:08:52 +0000

    ex 12, 13, 14, 15 y 16  completados

--- exercises/12-How_much_the_wedding_costs/app.js
+++ exercises/12-How_much_the_wedding_costs/app.js
@@ -1,12 +1,17 @@
 let guests = prompt('How many people are coming to your wedding?');
 
-function getPrice(guests){
+function getPrice(guests) {
     let cost = 0;
     // Your code here
- 
-
-    return cost;
+    if (guests <= 50) {
+        return cost + 4000;
+    } else if (guests <= 100) {
+        return cost + 10000;
+    } else if (guests <= 200) {
+        return cost + 15000;
+    } else {
+        return cost + 20000;
+    }
 }
-
-let price = getPrice(guests);
-console.log('Your wedding will cost '+price+' dollars');
+    let price = getPrice(guests);
+console.log('Your wedding will cost ' + price + ' dollars');
